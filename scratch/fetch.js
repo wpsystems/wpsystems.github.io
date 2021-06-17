7bedad1208cc8709106d49b86f5362f85116374c
wpsystems <pangjca@gmail.com>
2021-06-17 10:44:22 -0400

    turbo test

--- scratch/fetch.js
+++ scratch/fetch.js
@@ -1,67 +1,83 @@
-class ScratchFetch {
-    constructor() {
+class ScratchSimpleGamepad {
+    constructor(runtime) {
+        this.runtime = runtime
+        this.currentMSecs = -1
+        this.previousButtons = []
+        this.currentButtons = []
     }
     
     getInfo() {
         return {
-            "id": "Fetch",
-            "name": "Fetch",
-            "blocks": [
-                        {
-                            "opcode": "fetchURL",
-                            "blockType": "reporter",
-                            "text": "fetch data from [url]",
-                            "arguments": {
-                                "url": {
-                                    "type": "string",
-                                    "defaultValue": "https://api.weather.gov/stations/KNYC/observations"
-                                },
-                            }
+            "id": "SimpleGamepad",
+            "name": "SimpleGamepad",
+            "blocks": [{
+                        "opcode": "buttonPressedReleased",
+                        "blockType": "hat",
+                        "text": "button [b] [eventType]",
+                        "arguments": {
+                            "b": {
+                                "type": "number",
+                                "defaultValue": "0"
+                            },
+                            "eventType": {
+                                "type": "number",
+                                "defaultValue": "1",
+                                "menu": "pressReleaseMenu"
+                            },
                         },
-                        {
-                            "opcode": "jsonExtract",
-                            "blockType": "reporter",
-                            "text": "extract [name] from [data]",
-                            "arguments": {
-                                "name": {
-                                    "type": "string",
-                                    "defaultValue": "temperature"
-                                },
-                                "data": {
-                                    "type": "string",
-                                    "defaultValue": '{"temperature": 12.3}'
-                                },
-                            }
-                        },
-                ],
+                    },
+            ],
+            "menus": {
+                "pressReleaseMenu": [{text:"press",value:1}, {text:"release",value:0}],
+            }            
         };
     }
     
-    fetchURL({url}) {
-        return fetch(url).then(response => response.text())
-    }
-    
-    jsonExtract({name,data}) {
-
-        alert("hi");
-
-        return "hello there";
-        /*
-        var parsed = JSON.parse(data)
-        if (name in parsed) {
-            var out = parsed[name]
-            var t = typeof(out)
-            if (t == "string" || t == "number")
-                return out
-            if (t == "boolean")
-                return t ? 1 : 0
-            return JSON.stringify(out)
+    update() {
+        if (this.runtime.currentMSecs == this.currentMSecs) 
+            return // not a new polling cycle
+        this.currentMSecs = this.runtime.currentMSecs
+        var gamepads = navigator.getGamepads()
+        if (gamepads == null || gamepads.length == 0 || gamepads[0] == null) {
+            // different number of buttons, so new gamepad
+            this.previousButtons = []
+            this.currentButtons = []
+            return
+        }
+        var gamepad = gamepads[0]
+        if (gamepad.buttons.length != this.previousButtons.length) {
+            this.previousButtons = []
+            for (var i = 0; i < gamepad.buttons.length; i++) 
+                this.previousButtons.push(false)
         }
         else {
-            return ""
+            this.previousButtons = this.currentButtons
         }
-        */
+        this.currentButtons = []
+        for (var i = 0; i < gamepad.buttons.length; i++) 
+            this.currentButtons.push(gamepad.buttons[i].pressed)
+    }
+    
+    buttonPressedReleased({b,eventType}) {
+        this.update()
+        if (b < this.currentButtons.length) {
+            if (eventType == 1) { // note: this will be a string, so better to compare it to 1 than to treat it as a Boolean
+                if (this.currentButtons[b] && ! this.previousButtons[b]) {
+                    return true
+                }
+            }
+            else {
+                if (!this.currentButtons[b] && this.previousButtons[b]) {
+                    return true
+                }
+             }
+        }
+        return false
     }
 }
 
-Scratch.extensions.register(new ScratchFetch())
+(function() {
+    var extensionInstance = new ScratchSimpleGamepad(window.vm.extensionManager.runtime)
+    var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
+    window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
+})()
