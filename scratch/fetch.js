5822469f2e0b2226e08e4b0210127627aff6bcee
wpsystems <pangjca@gmail.com>
2021-06-17 09:37:56 -0400

    turbo test

--- scratch/fetch.js
+++ scratch/fetch.js
@@ -1,45 +1,61 @@
-class Fetch {
-    constructor () {
-      this.lastResponse = null;
+class ScratchFetch {
+    constructor() {
     }
-  
-    getInfo () {
-      return {
-        id: 'fetch',
-        name: 'Fetch',
-        blocks: [
-          {
-            opcode: 'get',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'GET [URL]',
-            arguments: {
-              URL: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'https://extensions.turbowarp.org/test'
-              }
-            }
-          }
-        ]
-      };
+    
+    getInfo() {
+        return {
+            "id": "Fetch",
+            "name": "Fetch",
+            "blocks": [
+                        {
+                            "opcode": "fetchURL",
+                            "blockType": "reporter",
+                            "text": "fetch data from [url]",
+                            "arguments": {
+                                "url": {
+                                    "type": "string",
+                                    "defaultValue": "https://api.weather.gov/stations/KNYC/observations"
+                                },
+                            }
+                        },
+                        {
+                            "opcode": "jsonExtract",
+                            "blockType": "reporter",
+                            "text": "extract [name] from [data]",
+                            "arguments": {
+                                "name": {
+                                    "type": "string",
+                                    "defaultValue": "temperature"
+                                },
+                                "data": {
+                                    "type": "string",
+                                    "defaultValue": '{"temperature": 12.3}'
+                                },
+                            }
+                        },
+                ],
+        };
     }
-  
-    _fetch (url) {
-      
-      return fetch(url)
-        .then(r => {
-          this.lastResponse = r;
-          return r;
-        });
+    
+    fetchURL({url}) {
+        return fetch(url).then(response => response.text())
     }
-  
-    get (args) {
-        
+    
+    jsonExtract({name,data}) {
+        var parsed = JSON.parse(data)
+        if (name in parsed) {
+            var out = parsed[name]
+            var t = typeof(out)
+            if (t == "string" || t == "number")
+                return out
+            if (t == "boolean")
+                return t ? 1 : 0
+            return JSON.stringify(out)
+        }
+        else {
+            return ""
+        }
+    }
+}
 
-      return this._fetch(args.URL)
-        .then(r => r.text())
-        .catch(() => '');
-        
-    }
-  }
-  
-  Scratch.extensions.register(new Fetch());
+Scratch.extensions.register(new ScratchFetch())
