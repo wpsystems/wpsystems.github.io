264e625929b7a7d32c28cafa6b4f94fa0d0b5daa
wpsystems <pangjca@gmail.com>
2021-06-26 18:36:55 -0400

    debug

---
+++ scratch/script.js
@@ -0,0 +1,30 @@
+ocument.addEventListener('DOMContentLoaded', () => {
+
+    alert("started");
+    const requests = [
+      { 
+        url: 'https://flaviocopes-cors-example-express.glitch.me/without-cors',
+        container: 'response-one'
+      }, { 
+        url: 'https://flaviocopes-cors-example-express.glitch.me/with-cors',
+        container: 'response-two'
+      }
+    ]
+  
+    for (const request of requests) {
+      fetch(request.url)
+      .then((response) => {
+        response.json().then((data) => {
+          document.getElementById(request.container)
+                  .getElementsByTagName('p')[0]
+                  .innerHTML = data.msg
+        })
+      })
+      .catch((err) => {
+        document.getElementById(request.container)
+                  .getElementsByTagName('p')[0]
+                  .innerHTML = 'CORS Error! 😯'
+      })
+    }
+    
+  }, false);
